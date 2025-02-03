4fca915b5c4e47758b63e4d711cbdb7a55e4b2cb
mohamedeidelshoura <mohamedeidelshoura@gmail.com>
2025-02-03 15:26:54 +0200

    first commit

---
+++ Views/TaskListViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+using ReactiveUI;
+
+namespace ProductivityApp.Views
+{
+    public class TaskViewModel : ReactiveObject
+    {
+        private string _title;
+        private bool _isCompleted;
+
+        public string Title
+        {
+            get => _title;
+            set => this.RaiseAndSetIfChanged(ref _title, value);
+        }
+
+        public bool IsCompleted
+        {
+            get => _isCompleted;
+            set => this.RaiseAndSetIfChanged(ref _isCompleted, value);
+        }
+    }
+
+    public class TaskListViewModel : ReactiveObject
+    {
+        public ObservableCollection<TaskViewModel> Tasks { get; } = new();
+
+        public ICommand AddTaskCommand { get; }
+
+        public TaskListViewModel()
+        {
+            AddTaskCommand = ReactiveCommand.Create(AddTask);
+        }
+
+        private void AddTask()
+        {
+            Tasks.Add(new TaskViewModel { Title = "مهمة جديدة", IsCompleted = false });
+        }
+    }
+}
